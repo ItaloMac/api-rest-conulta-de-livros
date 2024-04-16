e9a467d4aebd630447a42f36a62e5041f330cf31
italo macedo <macedoitalo21@gmail.com>
2024-04-16 14:52:45 -0500

    REquisição PUT Substituir Livro

--- src/controladores./livros.js
+++ src/controladores./livros.js
@@ -30,6 +30,37 @@ const criar = (req,res) => {
     return res.status(201).json(novoLivro)
 }
 
+const substituir = (req,res) => {
+    const livro = req.body
+
+    if(!livro.titulo || !livro.autor || !livro.ano || !livro.numPaginas) {
+        return res.status(400).json({
+            message: "Dados Invalidos"
+        })
+    }
+    
+    const {id} = req.params
+    
+    const livroExistente = livros.find((l) => l.id ===Number(id))
+    
+    if(!livroExistente) {
+        return res.status(404).json({
+            mensagem: "Nao existe livro a ser substituido para o id informado"
+        })
+    }
+    
+    livroExistente.titulo = livro.titulo
+    livroExistente.ano = livro.ano
+    livroExistente.autor = livro.autor
+    livroExistente.numPaginas = livro.numPaginas
+
+    const novoLivro = {id,...livro}
+    livros.push(novoLivro)
+    return res.status(201).json({
+        mensagem: "Livro substituido"
+    })
+}
+
 module.exports = {
     pegarTodos,
     pegarPeloId
